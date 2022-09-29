45a335e9b9e214143e86fc56a6d41d905a086158
meaux21 <maurice.grimes21@gmail.com>
2022-09-29 15:49:10 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ app.get('/greeting:name', (req,res)=>{
     const { name } = req.params
     res.send(`Hey, how is it going ${name}`)
 })
+
+app.get('/tip/:total/:tipPercentage', (req,res)=>{
+    const { total, tipPercentage } = req.params
+    res.send(`Here is your total = ${total}, and you the percentage you are tipping is ${tipPercentage}`)
+})
